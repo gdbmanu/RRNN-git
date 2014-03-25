4cbe57682be7c0085aa6fcc053dcf6aa56c3dabf
Dauce Emmanuel <edauce@scylla.etu.ec-m.fr>
2014-03-25 13:07:40 +0100

    modifs simu_cns_lif_1403.m

--- Simus/LIF/simu_cns_lif_1403.m
+++ Simus/LIF/simu_cns_lif_1403.m
@@ -1,9 +1,12 @@
 
-net = init_param_cns_lif(0.5); net = init_systeme_lif(net,60); % 8
+SEED = 60
+
+net = init_param_cns_lif(0.5); net = init_systeme_lif(net,SEED); % 8
 
 net = init_dyn_lif(net);
 
-net.FLAG_RENEWAL_RATE = 0;
+net.ENV_PERIOD = 60;
+net.ENV_RENEWAL_RATE = 0;
 
 net=iter_dyn_lif(net,2000,0);
 
@@ -11,4 +14,4 @@ net=iter_dyn_lif(net,6000,1);
 
 net=iter_dyn_lif(net,2000,0);
 
-save simu_cns_lif_1403_seed60_period60ms_RENEW00 net
+save simu_cns_lif_1403_seed60_tau10_period60ms_RENEW00 net
