1877d3070c62cb4de8f9eb881585fc9c84b65f4d
Dauce Emmanuel <edauce@gaby245.etu.ec-m.fr>
2014-03-25 15:01:16 +0100

    ajout path

--- Simus/LIF/simu_cns_lif_1403.m
+++ Simus/LIF/simu_cns_lif_1403.m
@@ -1,5 +1,6 @@
+path(path,genpath('/donnees/RRNN-git'))
 
-SEED = 60
+SEED = 245
 
 net = init_param_cns_lif(0.5); net = init_systeme_lif(net,SEED); % 8
 
@@ -14,4 +15,4 @@ net=iter_dyn_lif(net,6000,1);
 
 net=iter_dyn_lif(net,2000,0);
 
-save simu_cns_lif_1403_seed60_tau10_period60ms_RENEW00 net
+save simu_cns_lif_1403_seed245_tau10_period60ms_RENEW00 net
