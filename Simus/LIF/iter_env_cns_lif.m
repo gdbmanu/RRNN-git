ce1de51447155ad20c8017fbb41f9ad191afc8ba
dauce <dauce@port-dauce.pers.ec-m.fr>
2016-09-21 11:36:57 +0200

    ajout parametres dasn simu_cns_lif_1403

--- Simus/LIF/iter_env_cns_lif.m
+++ Simus/LIF/iter_env_cns_lif.m
@@ -62,14 +62,13 @@ if ((2 * pi * mod(net.t_out/period_1, 1)) > 3 * pi/2) & (net.FLAG_P_CHANGE == 0)
 end;
 
 
-FLAG_NON_NUL = 1;
-net.I{1} =   1 + FLAG_NON_NUL * net.A1 .* (cos(2 * pi * net.t_out / period_1 + net.phi1));
+net.I{1} =   1 + net.ENV_FLAG_NON_FLAT * net.A1 .* (cos(2 * pi * net.t_out / period_1 + net.phi1));
 %net.I{1} = net.I{1}.* (net.I{1} > 0);
 %net.I{1} = net.P11 * cos(2 * pi * net.t_out / period_1) + net.P12 * sin(2 * pi * net.t_out / period_1);
 %net.I{1} = (1 + sin(2 * pi * net.t_out / period_2)) * net.I{1};
 
 
-net.I{2} =   1 + FLAG_NON_NUL * net.A2 .* (cos(2 * pi * net.t_out / period_1 + net.phi2));
+net.I{2} =   1 + ENV_FLAG_NON_FLAT * net.A2 .* (cos(2 * pi * net.t_out / period_1 + net.phi2));
 %net.I{2} = net.I{2}.* (net.I{2} > 0);
 %net.I{2} = net.P21 * cos(2 * pi * net.t_out / period_1) + net.P22 * sin(2 * pi * net.t_out / period_1); 
 %net.I{2} = (1 + sin(2 * pi * net.t_out / period_2)) * net.I{2};
--- Simus/LIF/simu_cns_lif_1403.m
+++ Simus/LIF/simu_cns_lif_1403.m
@@ -1,26 +1,45 @@
-%path(path, genpath('~/RRNN-git'));
-path(path, genpath('/donnees/edauce/RRNN-git'));
+path(path, genpath('~/RRNN-git'));
+%path(path, genpath('/donnees/edauce/RRNN-git'));
 
 mem = {}
 for SEED = 1:1 % 8%60 %
 
-    net = init_param_cns_lif(0.5); net = init_systeme_lif(net,SEED); % 8
+    net = init_param_cns_lif(0.5); 
+    
+    % taille 
+    net.N = [1000;
+             400];
 
-    net = init_dyn_lif(net);
+    % type de neurone
+    net.FLAG_REV = 0; 
+    net.FLAG_SFA = 0;
+    
+    % type de delai
+    net.tau_d = 10;   %3; % (param/ fred) 6;%                                % D�lai moyen (ms)
+    net.nbp_d = floor(net.tau_d/net.delta_t);  % D�lai effectif (en pas de temps)
+    sigma_tau =  8;  % ecart-type en pas de temps
+    net.tau_min= (net.nbp_d - sigma_tau) * ones(net.nb_pop);             % net.tau_min : delai de transmission minimal                
+    net.tau_moy= sigma_tau * ones(2);   % parametre distri Poisson      
+    net.FLAG_UNIF = 1;                  % delais uniformes / Poisson    
 
+    % type d'input
     net.ENV_PERIOD = 30;
     net.ENV_RENEWAL_RATE = 1; %2/3; %2/3; %1; %2/3;
-
-    net.FLAG_SFA = 0;
+    net.ENV_FLAG_NON_FLAT = 0;
+    
+    % Param apprentissage
     net.alpha = 1;
     net.REWARD = 1;
-    
-    net=iter_dyn_lif(net,400,0);
+    % Optim si pas de plasticité
+    net.FLAG_DASHBOARD=0;
 
+    net = init_systeme_lif(net,SEED); % 8
+    net = init_dyn_lif(net);
+    net=iter_dyn_lif(net,2000,0);
 
-    net=iter_dyn_lif(net,10000,1);
+    %net=iter_dyn_lif(net,10000,1);
 
-    net=iter_dyn_lif(net,1000,0);
+    %net=iter_dyn_lif(net,1000,0);
 
     %mem{SEED} = net;
     
